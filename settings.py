f222973b16be78dbb8fc2c093a9fa4cc1010ebe4
hubi <krzyrzak1999@gmail.com>
2022-04-10 13:42:20 +0200

    first commit

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                                         ',
+'                                         ',
+'                                         ',
+'  XX      XXX             XX   XX        ',
+'  XX  P                                  ',
+'  XXXXX                XX                ',
+'  XXXXX           XX                     ',
+'  XX       X    XXXX        XX   XX      ',
+'          XX    XXXX   X    XX   XXX     ',
+'       XXXXX    XXXXXXXX    XX   XXXX    ',
+'  XXXXXXXXXX    XXXXXXXX    XX   XXXXX   ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
